df30930ff4326167717108d14f72b8e701dd6a77
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-03-11 07:32:34 -0500

    Finished lesson 104.

--- Pixel-City/Pixel-City/Utilities/Constants.swift
+++ Pixel-City/Pixel-City/Utilities/Constants.swift
@@ -9,3 +9,13 @@
 import Foundation
 
 let photoCellIdentifier = "photoCell"
+
+let apiKey = "57ddb451d9174f672e1b61658a6a4302"
+
+func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
+
+    let latitude = annotation.coordinate.latitude
+    let longitude = annotation.coordinate.longitude
+    
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(key)&lat=\(latitude)&lon=\(longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+}
